d70cfc18c086d90f4545ad7f2e2e62f1b9eb9be4
zohandoggo <arorarohan907@gmail.com>
2024-04-10 11:49:07 +0800

    planned out structure, made empty files

---
+++ main.py
@@ -0,0 +1,9 @@
+#this will be the script run by the user
+from taskmanager import TaskManager
+
+
+
+
+if __name__ == "__main__":
+    # manager = TaskManager()
+    pass
---
+++ taskmanager.py
@@ -0,0 +1,3 @@
+#this is to house the task manager class.
+class TaskManager:
+    pass
