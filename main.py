43cfc2b58d56161469ae9fb2099185e4b2fcadf3
zohandoggo <arorarohan907@gmail.com>
2024-04-10 13:41:24 +0800

    --

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from taskmanager import TaskManager
 
 
 
+#constants
+IMPORTANT_PATH = "lists\important.csv"
+GENERAL_PATH = "lists\general.csv"
+COMPLETED_PATH = "lists\completed.csv"
+
 
 if __name__ == "__main__":
-    # manager = TaskManager()
-    pass
+    manager = TaskManager(IMPORTANT_PATH,GENERAL_PATH,COMPLETED_PATH)
+    
